e65c2b0d65fc5eb06dc13f649c01996e35b30a88
AustinVanloon <55813021+AustinVanloon@users.noreply.github.com>
2025-04-17 16:32:04 -0400

    Made a Social page, will link friends together soon

---
+++ levelup-life-frontend/src/components/Social.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Social = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold">Welcome to your Friends List!</h1>
+    </div>
+  );
+};
+
+export default Social;
